4705a4a0c9b62a81331010227554ff2e6534863d
Mehradi <mehradi.info@gmail.com>
2022-11-13 07:59:16 +0200

    invoking command

--- cypress/e2e/ngx-admin/firstTest.cy.js
+++ cypress/e2e/ngx-admin/firstTest.cy.js
@@ -81,4 +81,39 @@ describe('our first suite',()=>{
          })
      })
    });
+
+
+   it('invoke command', () => {
+    cy.visit('http://localhost:4200/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    cy.get('[for="exampleInputEmail1"]')
+        .should('contain', 'Email address')
+        .should('have.class', 'label')
+        .and('have.text', 'Email address')
+
+    cy.get('[for="exampleInputEmail1"]').then( label => {
+        expect(label.text()).to.equal('Email address')
+        expect(label).to.have.class('label')
+        expect(label).to.have.text('Email address')
+    })
+
+    cy.get('[for="exampleInputEmail1"]').invoke('text').then( text => {
+        expect(text).to.equal('Email address')
+    })
+
+    cy.contains('nb-card', 'Basic form')
+        .find('nb-checkbox')
+        .click()
+        .find('.custom-checkbox')
+        .invoke('attr', 'class')
+        .then(classValue => {
+            expect(classValue).to.contain('checked')
+        })
+
+    });
+
+
+
 });
