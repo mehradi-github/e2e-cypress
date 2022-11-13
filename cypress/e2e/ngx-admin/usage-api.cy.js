e038833fc9c1e32fd3608958dab4e59c174eb644
Mehradi <mehradi.info@gmail.com>
2022-11-13 09:23:46 +0200

    Selecting day from current and test datepicker

--- cypress/e2e/ngx-admin/usage-api.cy.js
+++ cypress/e2e/ngx-admin/usage-api.cy.js
@@ -226,4 +226,38 @@ describe('our first suite',()=>{
 
 
     })
+
+
+    it.only('assert property', () => {
+
+        function selectDayFromCurrent(day){
+            let date = new Date()
+            date.setDate(date.getDate() + day)
+            let futureDay = date.getDate()
+            let futureMonth = date.toLocaleString('default', {month: 'short'})
+            let dateAssert = futureMonth+' '+futureDay+', '+date.getFullYear()
+            cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then( dateAttribute => {
+                if(!dateAttribute.includes(futureMonth)){
+                    cy.get('[data-name="chevron-right"]').click({force:true})
+                    selectDayFromCurrent(day)
+                } else {
+                    cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]').contains(futureDay).click()
+                }
+            })
+            return dateAssert
+        }
+
+        cy.visit('http://localhost:4200/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        cy.contains('nb-card', 'Common Datepicker').find('input').then( input => {
+            cy.wrap(input).click({force:true})
+            let dateAssert = selectDayFromCurrent(2)
+            cy.wrap(input).invoke('prop', 'value').should('contain', dateAssert)
+            cy.wrap(input).should('have.value', dateAssert)
+        })
+    })
+
+
 });
