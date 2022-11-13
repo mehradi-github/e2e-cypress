4f35779bee5258a2723542ed07813835e2c7c35d
Mehradi <mehradi.info@gmail.com>
2022-11-13 09:08:31 +0200

    Testing and edit web Table

--- cypress/e2e/ngx-admin/firstTest.cy.js
+++ cypress/e2e/ngx-admin/firstTest.cy.js
@@ -183,4 +183,47 @@ describe('our first suite',()=>{
         })
     })
 
+
+    
+    it('Web tables', () => {
+        cy.visit('http://localhost:4200/')
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+        cy.get('tbody').contains('tr', 'Larry').then( tableRow => {
+            cy.wrap(tableRow).find('.nb-edit').click({force:true})
+            cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25')
+            cy.wrap(tableRow).find('.nb-checkmark').click({force:true})
+            cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
+        })
+
+        cy.get('thead').find('.nb-plus').click({force:true})
+        cy.get('thead').find('tr').eq(2).then( tableRow => {
+            cy.wrap(tableRow).find('[placeholder="First Name"]').type('Artem')
+            cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Bondar')
+            cy.wrap(tableRow).find('.nb-checkmark').click({force:true})
+        })
+        cy.get('tbody tr').first().find('td').then( tableColumns => {
+            cy.wrap(tableColumns).eq(2).should('contain', 'Artem')
+            cy.wrap(tableColumns).eq(3).should('contain', 'Bondar')
+        })
+
+        const age = [20, 30, 40, 200]
+
+        cy.wrap(age).each( age => {
+            cy.get('thead [placeholder="Age"]').clear().type(age)
+            cy.wait(500)
+            cy.get('tbody tr').each( tableRow => {
+                if(age == 200){
+                    cy.wrap(tableRow).should('contain', 'No data found')
+                } else {
+                    cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+                }
+               
+            })
+        })
+
+
+
+    })
 });
