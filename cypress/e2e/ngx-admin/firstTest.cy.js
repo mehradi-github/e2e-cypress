7401f81cd806429605968e04b229fb0698914935
Mehradi <mehradi.info@gmail.com>
2022-11-13 08:45:21 +0200

    radio button

--- cypress/e2e/ngx-admin/firstTest.cy.js
+++ cypress/e2e/ngx-admin/firstTest.cy.js
@@ -116,4 +116,30 @@ describe('our first suite',()=>{
 
 
 
+
+    it('radio button', () => {
+        cy.visit('http://localhost:4200/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then( radioButtons => {
+            cy.wrap(radioButtons)
+                .first()
+                .check({force: true})
+                .should('be.checked')
+
+            cy.wrap(radioButtons)
+                .eq(1)
+                .check({force: true})
+                
+            cy.wrap(radioButtons)
+                .eq(0)
+                .should('not.be.checked')
+                
+            cy.wrap(radioButtons)
+                .eq(2)
+                .should('be.disabled')    
+        })
+    })
+
 });
