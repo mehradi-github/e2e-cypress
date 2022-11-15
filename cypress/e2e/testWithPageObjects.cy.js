64ad7e7626da06e8ae1ba85910bfa2bd0abb14ae
Mehradi <mehradi.info@gmail.com>
2022-11-15 14:53:35 +0200

    Testing and select tomorrow date in the calendar

--- cypress/e2e/testWithPageObjects.cy.js
+++ cypress/e2e/testWithPageObjects.cy.js
@@ -2,6 +2,7 @@
 
 import {navigateTo} from '../support/page-objects/navigationPage';
 import { onFormLayoutsPage } from "../support/page-objects/formLayoutsPage"
+import { onDatePickerPage } from "../support/page-objects/datepickerPage"
 
 describe('Test with Page Objects', () => {
 
@@ -9,10 +10,14 @@ describe('Test with Page Objects', () => {
         cy.openHomePage()
     })
 
-    it('should submit Inline and Basic form', () => {
+    it('should submit Inline and Basic form and select tomorrow date in the calendar', () => {
         navigateTo.formLayoutsPage()
         onFormLayoutsPage.submitInlineFormWithNameAndEmail('Artem', 'test@test.com')
         onFormLayoutsPage.submitBasicFormWithEmailAndPassword('test@test.com', 'password')
+
+        navigateTo.datepickerPage()
+        onDatePickerPage.selectCommonDatepickerDateFromToday(1)
+        onDatePickerPage.selectDatepickerWithRangeFromToday(7, 14)
      
     })
 });
--- cypress/support/page-objects/navigationPage.ts
+++ cypress/support/page-objects/navigationPage.ts
@@ -2,7 +2,7 @@ const selectGroupMenuItem=(groupName)=>{
     cy.contains('a', groupName).then( menu => {
         cy.wrap(menu).find('.expand-state g g').invoke('attr', 'data-name').then( attr => {
             if( attr.includes('left')){
-                cy.wrap(menu).click()
+                cy.wrap(menu).click({force: true})
             }
         })
     })
@@ -11,27 +11,27 @@ export class NavigationPage {
 
     formLayoutsPage(){
         selectGroupMenuItem('Form')
-        cy.contains('Form Layouts').click()
+        cy.contains('Form Layouts').click({force: true})
     }
 
     datepickerPage(){
         selectGroupMenuItem('Form')
-        cy.contains('Datepicker').click()
+        cy.contains('Datepicker').click({force: true})
     }
 
     toasterPage(){
         selectGroupMenuItem('Modal & Overlays')
-        cy.contains('Toastr').click()
+        cy.contains('Toastr').click({force: true})
     }
 
     smartTablePage(){
         selectGroupMenuItem('Tables & Data')
-        cy.contains('Smart Table').click()
+        cy.contains('Smart Table').click({force: true})
     }
 
     tooltipPage(){
         selectGroupMenuItem('Modal & Overlays')
-        cy.contains('Tooltip').click()
+        cy.contains('Tooltip').click({force: true})
     }
 }
 export const navigateTo = new NavigationPage();
